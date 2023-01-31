6ba4e11398c596efbcd32419e04bcec4681c042c
JackWesnitzer <jackwesnitzer@gmail.com>
2023-01-31 16:25:41 -0700

    ASCII Art 1st push

---
+++ Labs/ASCII_Art/main.py
@@ -0,0 +1,13 @@
+"""	
+	ASCII Art
+ 	Updated By: Jack Neckels-Wesnitzer
+ 	Date: 1/31/23
+	CSCI 110
+ 	This program prints ascii graphics on to the common output
+        /console/monitor.
+"""
+
+print('  |\\_/|   	**********************    	     (\\_/)  ')
+print(" / @ @ \\  	*           Lab 1    	*           (='.'=) ")
+print("" ( > 0 < )
+#FIXME: finish the rest of the art (80 points)
