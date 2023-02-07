a0aeb37c4f5a6a74790ce2054ea28d44450c6060
JackWesnitzer <jackwesnitzer@gmail.com>
2023-02-07 16:53:11 -0700

    Triangle second commit

--- Labs/Lab2-triangles/Triangles.py
+++ Labs/Lab2-triangles/Triangles.py
@@ -1,6 +1,7 @@
 # Created by: Jack Neckels-Wesnitzer
 # Date 2/7/23
-# Program info: Calculate the area and perimeter of a triangle using Heron's formula
+# Program info: Calculate the area and perimeter of a triangle using 
+# Heron's formula
 
 # Algorithm steps:
 # Input 3 sides of triangle, they should probably be floats
@@ -12,6 +13,20 @@
 
 import math
 
-side1 = float(input('enter the length of side1:'))
+side1 = float(input('enter the length of side1: '))
+side2 = float(input('enter the length of side2: '))
+side3 = float(input('enter the length of side3: '))
 
-print (math.sqrt(side1) )
+print ("side1 = ", side1)
+print ("side2 = ", side2)
+print ("side3 = ", side3)
+
+if (side1+side2) < side3 or (side3+side2) < side2 or (side3+side2) < side1:
+    print ("invalid triangle")
+else:
+    perimeter = side1+side2+side3
+    semi_perimeter = perimeter/2
+    area = math.sqrt( semi_perimeter*(semi_perimeter-side1)*(semi_perimeter-side2)*(semi_perimeter-side3) )
+
+print('The perimeter is ' , perimeter)
+print(' The area is ', area )
