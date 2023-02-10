7ae89a21ab63e8fcdd4009a99cc85d2a200ba4b7
JackWesnitzer <jackwesnitzer@gmail.com>
2023-02-10 16:34:17 -0700

    stdio initial commit

---
+++ assignments/stdio/stdio.py
@@ -0,0 +1,8 @@
+# written by Jack K Neckels-Wesnitzer
+# Date 02/10/23
+
+# standard input and output program that prints four playing cards in ASCII and/or uniCODE
+# begins by greeting the player and asking their name
+# player inputs their name
+# program calls them by name and introduces ASCII cards
+
