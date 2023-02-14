5ba472820fdf4910dfa8073b9cee6084edbd7f75
JackWesnitzer <jackwesnitzer@gmail.com>
2023-02-13 17:01:49 -0700

    Input_Output_Commit

--- assignments/stdio/stdio.py
+++ assignments/stdio/stdio.py
@@ -6,3 +6,41 @@
 # player inputs their name
 # program calls them by name and introduces ASCII cards
 
+def greeting():
+    name = input('Hello there, what is your name?: ')
+    return name
+
+name = greeting()
+print('Hey,', name)
+print('Here are a few playing cards that I created. Maybe I will make a card game!')
+
+print(" ========")
+print("| A      |")
+print("|  /\\/\\  |")
+print("|  \\  /  |")
+print("|   \\/   |")
+print("|      A |")
+print(" ========")
+
+print("Ace of Hearts")
+
+print(" ========")
+print("| A      |")
+print("|   /\\   |")
+print("|  /  \\  |")
+print("|  \\  /  |")
+print("|   \\/   |")
+print("|      A |")
+print(" ========")
+
+print("Ace of Diamonds")
+
+print(" ========")
+print("| A      |")
+print("|  (  )  |")
+print("| (_  _) |")
+print("|   /\\   |")
+print("|      A |")
+print(" ========")
+
+print("Ace of Clubs")
