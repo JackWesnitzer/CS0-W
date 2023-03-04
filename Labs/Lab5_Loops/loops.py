4c32cdadd6e2e5f6245fb69645aeec8aecf3888f
JackWesnitzer <jackwesnitzer@gmail.com>
2023-03-03 17:04:13 -0700

    Loops_Final_Commit

--- Labs/Lab5_Loops/loops.py
+++ Labs/Lab5_Loops/loops.py
@@ -8,7 +8,6 @@ Program prints geometric shapes of given height with * using loops
 import os
 import sys
 
-
 def printTriangle(height):
     """
     Function takes height as an argument to print the triangle
@@ -19,7 +18,6 @@ def printTriangle(height):
         print('*  '*i)
         i += 1
     print()  # print an empty line
-
 
 def printFlippedTriangle(height):
     """
@@ -37,12 +35,14 @@ def printFlippedTriangle(height):
         print('*  '*i)
         i -= 1
     print()
+
     # FIXME3 ... - #fixed#
 
 # FIXME4 - #fixed#
 # Design and implement a function that takes a number as height and
 # prints square of the given height with *.
 # Square of height 5, e.g., would look like the following.
+
 def printSquare(height):
 
     i = height
@@ -70,45 +70,50 @@ def clearScreen():
 
 def main():
     
-    # FIXME8 call clearScreen function to clear the screen for each round of the loop
-    #clearScreen()
-    clearScreen()
-
-    # FIXME9 - #fixed#
-    # prompt user to enter y/Y to continue anything else to quit
-
-    print('Program prints geometric shapes of given height with * ...')
-    ans = input('Would you like to continue? [y/n} ') 
-    if ans == 'y':
-        height = int(input('Please enter the height of the shape: '))
-    if ans == 'n':
-        print('Goodbye!')
-        sys.exit
-
-    # call printTriangle function passing user entered height
-    printTriangle(height)
-
-    # FIXME5 - #fixed#
-    # Call printFlippedTriangle passing proper argument
-    # Manually test the function
-    printFlippedTriangle(height)
-
-    # FIXME6 - #fixed#
-    # Call the function defined in FIXME4 passing proper argument
-    # Manually test the function
-    printSquare(height)
-
-    # FIXME7 add a loop to make the program to continue to run until the user wants to quit 
-
-    answer = input("Would you like to check another number? Enter y to continue; anything else to quit: ")
-    if answer != 'y':
-        print('Thanks for using the program!')
-        sys.exit()
-
-    # FIXME8 call clearScreen function to clear the screen for each round of the loop
+    # FIXME8 call clearScreen function to clear the screen for each round of the loop - #fixed#
     
-    # FIXME10
-    # Use conditional statements to break the loop or continue in the loop
+    while True:
+
+        clearScreen()
+
+        # FIXME9 - #fixed#
+        # prompt user to enter y/Y to continue anything else to quit
+
+        print('Program prints geometric shapes of given height with * ...')
+        
+        height = int(input('Please enter the height of the shape: '))
+
+        # call printTriangle function passing user entered height
+
+        printTriangle(height)
+
+        # FIXME5 - #fixed#
+        # Call printFlippedTriangle passing proper argument
+        # Manually test the function
+        
+        printFlippedTriangle(height)
+
+        # FIXME6 - #fixed#
+        # Call the function defined in FIXME4 passing proper argument
+        # Manually test the function
+        
+        printSquare(height)
+
+        # FIXME7 add a loop to make the program to continue to run until the user wants to quit - #fixed#
+
+        answer = input("Would you like to check another number? Enter y to continue; anything else to quit: ")
+
+        if answer == 'y':
+                continue
+        if answer != 'y':
+                clearScreen()
+                print('Thanks for using the program! Goodbye!')
+                break
+
+        # FIXME8 call clearScreen function to clear the screen for each round of the loop - #fixed#
+        
+        # FIXME10 - #fixed#
+        # Use conditional statements to break the loop or continue in the loop
 
 if __name__ == "__main__":
     main()
